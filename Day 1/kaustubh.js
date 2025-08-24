7cc247eb5a928c475885ecd10349c83904b97736
kaustubh Bajpai <144876616+Kaustubhbajpai7777@users.noreply.github.com>
2025-08-24 23:21:36 +0530

    Create kaustubh.js

---
+++ Day 1/kaustubh.js
@@ -0,0 +1,12 @@
+const car = {
+    brand: "Tesla",
+    model: "Model 3",
+    year: 2024,
+    color: "Midnight Silver"
+};
+
+console.log("🚘 Car Details : ");
+console.log("Brand:", car.brand);
+console.log("Model:", car.model);
+console.log("Year:", car.year);
+console.log("Color:", car.color);
