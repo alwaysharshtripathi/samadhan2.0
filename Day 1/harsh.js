4f422ca632eba7f495625af053db2252e2295b34
Naitik Tiwari <harshtripathi3020@gmail.com>
2025-08-24 23:10:18 +0530

    Added Day 1 tasks

---
+++ Day 1/harsh.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Harsh Tripathi",
+    age: 21,
+    course: "B.Tech CSE",
+    college: "Amity University Gwalior"
+};
+
+console.log("📌 Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("College:", student.college);
