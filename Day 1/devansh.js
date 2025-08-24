6ce03756dcf42e96e26870908c1d444abf6b8e7d
Devansh Shrivastava <145153202+devanshshrivastava16@users.noreply.github.com>
2025-08-24 23:12:18 +0530

    devansh.js

---
+++ Day 1/devansh.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Devansh Shrivastava",
+    age: 19,
+    course: "B.Tech CSE",
+    college: "Amity University Gwalior"
+};
+
+console.log("📌 Student Details:");
+console.log("Name is ", student.name);
+console.log("Age is ", student.age);
+console.log("Course is ", student.course);
+console.log("College is ", student.college);
