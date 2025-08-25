20a775b7690b235358b0866499635c145d090287
Naitik Tiwari <harshtripathi3020@gmail.com>
2025-08-25 14:20:48 +0530

    Added Day 2 task

---
+++ Day 2/harsh.js
@@ -0,0 +1,14 @@
+let marks = [78, 92, 85, 67, 99, 74];
+
+function findHighest(arr) {
+    let highest = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > highest) {
+            highest = arr[i];
+        }
+    }
+    return highest;
+}
+
+let topMarks = findHighest(marks);
+console.log("🏆 Highest Marks:", topMarks);
