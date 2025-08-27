a63b19b70124bc0b1de3068c6bf6c59f31ee5202
Naitik Tiwari <harshtripathi3020@gmail.com>
2025-08-27 23:17:47 +0530

    Added Day 4 tasks

---
+++ Day 4/harsh_node-api/nodejs_intro/server.js
@@ -0,0 +1,19 @@
+// Import http module
+const http = require('http');
+
+// Create server
+const server = http.createServer((req, res) => {
+  // Set response header
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+
+  // Return JSON response
+  res.end(JSON.stringify({ message: "Hello, World!" }));
+});
+
+// Define port
+const PORT = 3000;
+
+// Start server
+server.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}/`);
+});
