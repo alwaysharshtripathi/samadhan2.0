69d6b2acc50c2158d13f37f83455f295f4fafe68
Devansh Shrivastava <145153202+devanshshrivastava16@users.noreply.github.com>
2025-08-30 00:08:08 +0530

    Add files via upload

---
+++ Day 6/my-profile-devansh/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfileCard from "./components/Components/ProfileCard/ProfileCard";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;
